a743fa801912021323be7bece3edb9444ccec26a
Ashmitajha <87004218+Ashmitajha@users.noreply.github.com>
2024-09-09 03:58:06 +0530

    remove category prop

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import Home from './components/Home'
 import Quiz from './components/Quiz'
-import { QuizCategories } from './constants/quiz-categories'
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +10,6 @@ export const router = createBrowserRouter([
   },
   {
     path: '/quiz',
-    element: <Quiz category={QuizCategories[1]} />,
+    element: <Quiz />,
   },
 ])
